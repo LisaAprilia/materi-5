de1898da7bd9660bca8b415cd7fd870aff548dd8
LISA <aprilialisa654@gmail.com>
2020-11-18 06:09:28 +0700

    Initial commit

---
+++ materi-4/system/resources/views/register.blade.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>LalisaShop</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="robots" content="all,follow">
+     
+    <!-- Font Awesome -->
+  <link rel="stylesheet" href="{{ url ('public') }} /plugins/fontawesome-free/css/all.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- icheck bootstrap -->
+  <link rel="stylesheet" href="{{ url ('public') }} /plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="{{ url ('public') }} /dist/css/adminlte.min.css">
+  <!-- Google Font: Source Sans Pro -->
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+  <body>
+    <!-- navbar-->
+    <header class="header mb-5">
+      <!--
+      * TOPBAR *
+      ___________________
+      -->
+     
+    </header>
+    <div id="all">
+      <div id="content">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-12">
+              <!-- breadcrumb-->
+              <nav aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                 
+                </ol>
+              </nav>
+            </div>
+            <div class="col-lg-6">
+              <div class="box">
+                <h1>New account</h1>
+                <p class="lead">Not our registered customer yet?</p>
+                <p>With registration with us new world of fashion, fantastic discounts and much more opens to you! The whole process will not take you more than a minute!</p>
+                <p class="text-muted">If you have any questions, please feel free to <a href="contact.html">contact us</a>, our customer service center is working for you 24/7.</p>
+                <hr>
+                <form action="admin/produk/createe" method="post">
+                  <div class="form-group">
+                    <label for="name">Name</label>
+                    <input id="name" type="text" class="form-control">
+                  </div>
+                  <div class="form-group">
+                    <label for="email">Email</label>
+                    <input id="email" type="text" class="form-control">
+                  </div>
+                  <div class="form-group">
+                    <label for="password">Password</label>
+                    <input id="password" type="password" class="form-control">
+                  </div>
+                  <div class="text-center">
+                    <button type="submit" class="btn btn-primary"><i class="fa fa-user-md"></i> Register</button>
+                  </div>
+                </form>
+              </div>
+            </div>
+            <div class="col-lg-6">
+              <div class="box">
+                <h1>Login</h1>
+                <p class="lead">Already our customer?</p>
+                <p class="text-muted">Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
+                <hr>
+                <form action="customer-orders.html" method="post">
+                  <div class="form-group">
+                    <label for="email">Email</label>
+                    <input id="email" type="text" class="form-control">
+                  </div>
+                  <div class="form-group">
+                    <label for="password">Password</label>
+                    <input id="password" type="password" class="form-control">
+                  </div>
+                  <div class="text-center">
+                    <button type="submit" class="btn btn-primary"><i class="fa fa-sign-in"></i> Log in</button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <br>
+    <br>
+    <br>
+     <div class="row pt-5 mt-5 text-center">
+          <div class="col-md-12">
+            <p>
+            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+            Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart" aria-hidden="true"></i> by <a href="https://www.instagram.com/lisaapr_30/?hl=id" target="_blank" class="text-primary">Lisa Aprilia</a>
+            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+            </p>
+          </div>
+          
+        </div>
+      </div>
+    </footer>
+  </div>
+    <!-- * COPYRIGHT END *-->
+    <!-- JavaScript files-->
+    <script src="{{ url ('public') }} /vendor/jquery/jquery.min.js"></script>
+    <script src="{{ url ('public') }} /vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="{{ url ('public') }} /vendor/jquery.cookie/jquery.cookie.js"> </script>
+    <script src="{{ url ('public') }} /vendor/owl.carousel/owl.carousel.min.js"></script>
+    <script src="{{ url ('public') }} /vendor/owl.carousel2.thumbs/owl.carousel2.thumbs.js"></script>
+    <script src="{{ url ('public') }} /js/front.js"></script>
+  </body>
+</html>
